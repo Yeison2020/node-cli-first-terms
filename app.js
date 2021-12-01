f745a32362a928b90f69ed3e4c8bd63a49e5dae1
Yeison <enmanuelquiterios@gmail.com>
2021-12-01 12:40:57 -0500

    Add fs write files system

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-console.clear();
+// Importing files from node
+const fs = require("fs");
 
+console.clear();
 console.log("Working with CLI node first steps");
 
 console.log("==================================");
 
 const base = 5;
+
+let salida = "";
 for (let i = 1; i <= 10; i++) {
-  console.log(`${base} x ${i} = ${base * i}`);
+  salida += `${base} x ${i} = ${base * i}\n`;
 }
+
+// Here name_file | data that goes inside of the file | checking errors during process
+fs.writeFile("table-5.txt", salida, (error) => {
+  if (error) throw error;
+  console.log("Table have been created");
+});
+console.log(salida);
