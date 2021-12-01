9dae47f68e503174259653c2d4f55fe919c5a9f1
Yeison <enmanuelquiterios@gmail.com>
2021-12-01 12:29:08 -0500

    First practice in node CLI

---
+++ app.js
@@ -0,0 +1,10 @@
+console.clear();
+
+console.log("Working with CLI node first steps");
+
+console.log("==================================");
+
+const base = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${base} x ${i} = ${base * i}`);
+}
